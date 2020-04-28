937d1a39ed7750593979fe71de48dc61ab2dbf7d
Jake Hendy <me@jakehendy.com>
2020-04-28 22:11:34 +0100

    Create project

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ const fs = require('fs');
 
 let projects = fs.readdirSync('./projects');
 console.log(`We have ${projects.length} project(s)`);
+const OWNER_REPO = (process.env.GITHUB_REPOSITORY || "JakeHendy/onboarding-repo");
+const OWNER = OWNER_REPO.split('/')[0];
+const REPO = OWNER_REPO.split('/')[1];
+const octo = new Octokit({
+    auth: process.env.GITHUB_TOKEN
+})
 
 projects.forEach(project => {
     let metadataPath = `./projects/${project}/metadata.json`;
     let cards = fs.readdirSync(`./projects/${project}`);
     let hasMetadata = fs.existsSync(`./projects/${project}/metadata.json`);
-    let metadata = hasMetadata ? fs.readFileSync(metadataPath) : { "name" : project };
+    let metadata = hasMetadata ? JSON.parse(fs.readFileSync(metadataPath)) : { "name" : project };
     let numberOfCards = hasMetadata ? cards.length - 1 : cards.length;
     console.log(`${project} has ${numberOfCards} card(s)`);
-    
+
+    let projectCreated = octo.projects.createForRepo({
+        owner: OWNER,
+        repo: REPO,
+        name: metadata['name'],
+    }).catch(e => console.log(e));
 })
